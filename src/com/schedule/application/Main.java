609f7ee5a77bb03c276c2ae9f73fc2e5efbdf29e
Ryan Iug <ryaniugramos@gmail.com>
2023-08-07 23:00:26 -0300

    delete

--- src/com/schedule/application/Main.java
+++ src/com/schedule/application/Main.java
@@ -10,11 +10,25 @@ public class Main {
         ContactDAO contactDao = new ContactDAO();
 
         Contact contact1 = new Contact();
-        contact1.setName("Pedro Fabri");
-        contact1.setAge(29);
+        contact1.setName("Jonas Pedrolas");
+        contact1.setAge(25);
         contact1.setRegisterDate(new Date());
 
-        contactDao.save(contact1);
+        //contactDao.save(contact1);
+
+        Contact contact1Up = new Contact();
+        contact1Up.setName("Ryan Iug Ramos");
+        contact1Up.setAge(22);
+        contact1Up.setRegisterDate(new Date());
+        contact1Up.setId(1);
+
+        //contactDao.update(contact1Up);
+        contactDao.delete(4);
+
+
+        for (Contact c : contactDao.getContacts()) {
+            System.out.println("Contato: " + c.getName() + ", " + c.getAge() + " anos, " + c.getRegisterDate());
+        }
     }
 
 }
--- src/com/schedule/dao/ContactDAO.java
+++ src/com/schedule/dao/ContactDAO.java
@@ -6,6 +6,10 @@ import com.schedule.model.Contact;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactDAO {
     public void save(Contact contact) {
@@ -22,6 +26,109 @@ public class ContactDAO {
             pstm.setDate(3, new Date(contact.getRegisterDate().getTime()));
 
             pstm.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstm != null) {
+                    pstm.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<Contact> getContacts() {
+        String sql = "SELECT * FROM contacts";
+        List<Contact> contacts = new ArrayList<Contact>();
+        Connection connection = null;
+        PreparedStatement pstm = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = ConnectionFactory.createConnectionToMySQL();
+            pstm = (PreparedStatement) connection.prepareStatement(sql);
+            resultSet = pstm.executeQuery();
+
+            while (resultSet.next()) {
+                Contact contact = new Contact();
+                contact.setId(resultSet.getInt("id"));
+                contact.setName(resultSet.getString("name"));
+                contact.setAge(resultSet.getInt("age"));
+                contact.setRegisterDate(resultSet.getDate("registerDate"));
+
+                contacts.add(contact);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstm != null) {
+                    pstm.close();
+                }
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return contacts;
+    }
+
+    public void update(Contact contact) {
+        String sql = "UPDATE contacts SET name = ?, age = ?, registerDate = ? " + "WHERE id = ?";
+        Connection connection = null;
+        PreparedStatement pstm = null;
+
+        try {
+            connection = ConnectionFactory.createConnectionToMySQL();
+            pstm = (PreparedStatement) connection.prepareStatement(sql);
+
+            pstm.setString(1, contact.getName());
+            pstm.setInt(2, contact.getAge());
+            pstm.setDate(3, new Date(contact.getRegisterDate().getTime()));
+            pstm.setInt(4, contact.getId());
+
+            pstm.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstm != null) {
+                    pstm.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void delete(int id) {
+        String sql = "DELETE FROM contacts WHERE id = ?";
+        Connection connection = null;
+        PreparedStatement pstm = null;
+
+        try {
+            connection = ConnectionFactory.createConnectionToMySQL();
+            pstm = (PreparedStatement) connection.prepareStatement(sql);
+            pstm.setInt(1, id);
+            pstm.execute();
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
